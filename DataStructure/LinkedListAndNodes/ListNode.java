03bc19a454534aca180889f30ff98b40debea628
mehmetrecep <mehmetrecep650@gmail.com>
2023-03-19 02:14:55 +0300

    adding constructor of ListNode class

--- DataStructure/LinkedListAndNodes/ListNode.java
+++ DataStructure/LinkedListAndNodes/ListNode.java
@@ -1,5 +1,31 @@
 package DataStructureX.Algorthims.DataStructure.LinkedListAndNodes;
 
 public class ListNode {
+    private int value;
+    private ListNode next;
 
+    public ListNode(int value, ListNode next){
+        this.next = next;
+        this.value = value;
+    }
+    public ListNode(int value){
+        this.value = value;
+        this.next = null;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
 }
