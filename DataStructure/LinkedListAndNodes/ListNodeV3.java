f9641d95069b44bca24a040a51291f7c5788c5ba
mehmetrecep <mehmetrecep650@gmail.com>
2023-04-15 08:58:15 +0300

    updated

---
+++ DataStructure/LinkedListAndNodes/ListNodeV3.java
@@ -0,0 +1,18 @@
+package DataStructureX.Algorthims.DataStructure.LinkedListAndNodes;
+
+public class ListNodeV3 {
+    int val;
+    ListNodeV3 next;
+    ListNodeV3(){}
+    ListNodeV3(int val){
+        this.val = val;
+    }
+    ListNodeV3(int val, ListNodeV3 next){
+        this.val = val;
+        this.next = next;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
